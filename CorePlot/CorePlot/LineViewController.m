a91483c39d11dca3d7fbe90ba7f6e3a17db5258e
wayne <zixiao.wang@cz-tek.com>
2014-04-14 18:19:05 +0800

    w

--- CorePlot/CorePlot/LineViewController.m
+++ CorePlot/CorePlot/LineViewController.m
@@ -11,7 +11,7 @@
 @interface LineViewController ()<CPTPlotDataSource,CPTAxisDelegate>
 @property (nonatomic,strong) NSMutableArray *dataArray;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *hostViewHeight;
-@property (weak, nonatomic) IBOutlet CPTGraphHostingView *hostView;
+@property (strong, nonatomic) IBOutlet CPTGraphHostingView *hostView;
 
 @end
 
@@ -29,10 +29,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.dataArray=[[NSMutableArray alloc] init];
-    for(int i=0;i<10;i++){
-        [self.dataArray addObject:[NSNumber numberWithInt:arc4random()%20+1]];
-    }
+    [self constructScatterPlot];
+//    self.dataArray=[[NSMutableArray alloc] init];
+//    for(int i=0;i<10;i++){
+//        [self.dataArray addObject:[NSNumber numberWithInt:arc4random()%20+1]];
+//    }
     // Do any additional setup after loading the view.
 }
 
@@ -57,29 +58,131 @@
 //        self.hostViewHeight.constant=self.view.frame.size.height/2 ;
 //    }
 }
+-(void)constructScatterPlot
+{
+    //在CPTGragh中画图，CPTXYGraph是个曲线图
+    CGRect frame=self.hostView.frame;
+    self.hostView=[[CPTGraphHostingView alloc] initWithFrame:frame];
+    CPTXYGraph *graph=[[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    if(graph){
+        CPTTheme *theme = [CPTTheme themeNamed:kCPTDarkGradientTheme];
+        [graph applyTheme:theme];
+        self.hostView.hostedGraph=graph;
+        
+        graph.paddingLeft   = 10.0;
+        graph.paddingTop    = 10.0;
+        graph.paddingRight  = 10.0;
+        graph.paddingBottom = 10.0;
+        
+        
+        // Setup plot space
+        CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
+        plotSpace.allowsUserInteraction = YES;
+        plotSpace.xRange                = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(1.0) length:CPTDecimalFromDouble(2.0)];
+        plotSpace.yRange                = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(1.0) length:CPTDecimalFromDouble(3.0)];
+        
+        // Axes
+        CPTXYAxisSet *axisSet = (CPTXYAxisSet *)graph.axisSet;
+        CPTXYAxis *x          = axisSet.xAxis;
+        x.majorIntervalLength         = CPTDecimalFromDouble(0.5);
+        x.orthogonalCoordinateDecimal = CPTDecimalFromDouble(2.0);
+        x.minorTicksPerInterval       = 2;
+        NSArray *exclusionRanges = @[[CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(1.99) length:CPTDecimalFromDouble(0.02)],
+                                     [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(0.99) length:CPTDecimalFromDouble(0.02)],
+                                     [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(2.99) length:CPTDecimalFromDouble(0.02)]];
+        x.labelExclusionRanges = exclusionRanges;
+        
+        CPTXYAxis *y = axisSet.yAxis;
+        y.majorIntervalLength         = CPTDecimalFromDouble(0.5);
+        y.minorTicksPerInterval       = 5;
+        y.orthogonalCoordinateDecimal = CPTDecimalFromDouble(2.0);
+        exclusionRanges               = @[[CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(1.99) length:CPTDecimalFromDouble(0.02)],
+                                          [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(0.99) length:CPTDecimalFromDouble(0.02)],
+                                          [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(3.99) length:CPTDecimalFromDouble(0.02)]];
+        y.labelExclusionRanges = exclusionRanges;
+        
+        // Create a green plot area
+        CPTScatterPlot *dataSourceLinePlot = [[CPTScatterPlot alloc] init];
+        dataSourceLinePlot.identifier = @"Green Plot";
+        
+        CPTMutableLineStyle *lineStyle = [dataSourceLinePlot.dataLineStyle mutableCopy];
+        lineStyle.lineWidth              = 3.0;
+        lineStyle.lineColor              = [CPTColor greenColor];
+        lineStyle.dashPattern            = @[@5.0f, @5.0f];
+        dataSourceLinePlot.dataLineStyle = lineStyle;
+        
+        dataSourceLinePlot.dataSource = self;
+        
+        // Put an area gradient under the plot above
+        CPTColor *areaColor       = [CPTColor colorWithComponentRed:0.3 green:1.0 blue:0.3 alpha:0.8];
+        CPTGradient *areaGradient = [CPTGradient gradientWithBeginningColor:areaColor endingColor:[CPTColor clearColor]];
+        areaGradient.angle = -90.0;
+        CPTFill *areaGradientFill = [CPTFill fillWithGradient:areaGradient];
+        dataSourceLinePlot.areaFill      = areaGradientFill;
+        dataSourceLinePlot.areaBaseValue = CPTDecimalFromDouble(1.75);
+        
+        // Animate in the new plot, as an example
+        dataSourceLinePlot.opacity        = 0.0;
+        dataSourceLinePlot.cachePrecision = CPTPlotCachePrecisionDecimal;
+        [graph addPlot:dataSourceLinePlot];
+        
+        CABasicAnimation *fadeInAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+        fadeInAnimation.duration            = 1.0;
+        fadeInAnimation.removedOnCompletion = NO;
+        fadeInAnimation.fillMode            = kCAFillModeForwards;
+        fadeInAnimation.toValue             = @1.0;
+        [dataSourceLinePlot addAnimation:fadeInAnimation forKey:@"animateOpacity"];
+        
+        // Create a blue plot area
+        CPTScatterPlot *boundLinePlot = [[CPTScatterPlot alloc] init];
+        boundLinePlot.identifier = @"Blue Plot";
+        
+        lineStyle            = [boundLinePlot.dataLineStyle mutableCopy];
+        lineStyle.miterLimit = 1.0;
+        lineStyle.lineWidth  = 3.0;
+        lineStyle.lineColor  = [CPTColor blueColor];
+        
+        boundLinePlot.dataSource     = self;
+        boundLinePlot.cachePrecision = CPTPlotCachePrecisionDouble;
+        boundLinePlot.interpolation  = CPTScatterPlotInterpolationHistogram;
+        [graph addPlot:boundLinePlot];
+        
+        // Do a blue gradient
+        CPTColor *areaColor1       = [CPTColor colorWithComponentRed:0.3 green:0.3 blue:1.0 alpha:0.8];
+        CPTGradient *areaGradient1 = [CPTGradient gradientWithBeginningColor:areaColor1 endingColor:[CPTColor clearColor]];
+        areaGradient1.angle         = -90.0;
+        areaGradientFill            = [CPTFill fillWithGradient:areaGradient1];
+        boundLinePlot.areaFill      = areaGradientFill;
+        boundLinePlot.areaBaseValue = [[NSDecimalNumber zero] decimalValue];
+        
+        // Add plot symbols
+        CPTMutableLineStyle *symbolLineStyle = [CPTMutableLineStyle lineStyle];
+        symbolLineStyle.lineColor = [CPTColor blackColor];
+        CPTPlotSymbol *plotSymbol = [CPTPlotSymbol ellipsePlotSymbol];
+        plotSymbol.fill          = [CPTFill fillWithColor:[CPTColor blueColor]];
+        plotSymbol.lineStyle     = symbolLineStyle;
+        plotSymbol.size          = CGSizeMake(10.0, 10.0);
+        boundLinePlot.plotSymbol = plotSymbol;
+        
+        // Add some initial data
+        NSMutableArray *contentArray = [NSMutableArray arrayWithCapacity:100];
+        for ( NSUInteger i = 0; i < 60; i++ ) {
+            NSNumber *x = @(1 + i * 0.05);
+            NSNumber *y = @(1.2 * rand() / (double)RAND_MAX + 1.2);
+            [contentArray addObject:@{ @"x": x, @"y": y }];
+        }
+        self.dataArray = contentArray;
+        
+        
+    }
+
+}
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     //图形要放在一个CPTGraphHosting中(uiview)
     [self fixOrientation];
-    //在CPTGragh中画图，CPTXYGraph是个曲线图
-    CPTXYGraph *graph=[[CPTXYGraph alloc] initWithFrame:self.hostView.frame];
-    if(graph){
-        self.hostView.hostedGraph=graph;
-        CPTScatterPlot *scatter=[[CPTScatterPlot alloc] initWithFrame:graph.bounds];
-        [graph addPlot:scatter];
-        scatter.dataSource=self;
-        
-        //设置 PlotSpace，这里的 xRange 和 yRange 要理解好，它决定了点是否落在图形的可见区域
-        //location 值表示坐标起始值，一般可以设置元素中的最小值
-        //length 值表示从起始值上浮多少，一般可以用最大值减去最小值的结果
-        CPTXYPlotSpace *plotSpace=(CPTXYPlotSpace *)scatter.plotSpace;
-        plotSpace.xRange=[CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0)
-                                                      length:CPTDecimalFromFloat([self.dataArray count]-1)];
-        plotSpace.yRange=[CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0)
-                                                      length:CPTDecimalFromFloat(20)];
-    }
-   
+    
 
 }
 -(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
